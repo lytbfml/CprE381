354a5cdc4e2ff6970a180c961845fd2a819d48e4
yangxiao AT iastate.edu <yangxiao@iastate.edu>
2016-11-06 21:28:17 -0600

    test2

--- ProjectB/P1/controlS.vhd
+++ ProjectB/P1/controlS.vhd
@@ -15,7 +15,7 @@ entity controlS is
   port (
         opCode     	: in std_logic_vector(5 downto 0);
 	funct		: in std_logic_vector(5 downto 0);
-		rt			: in std_logic_vector(4 downto 0);
+	rt			: in std_logic_vector(4 downto 0);
         RegDst     	: out std_logic;
 		ALUSrc		: out std_logic;
         MemtoReg    : out std_logic;
